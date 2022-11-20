a37335c16f766b39eac295c4e7a58b5af66a26e7
Nick Morris <morrinic@oregonstate.edu>
2022-11-19 17:14:50 -0800

    Added JQuery onclick function to target specific element

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,8 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var clickedBtn = $('.saveBtn');
 $(function (
-
-  
-
-
-
-
-
-
-
-
-
-
-
 
 ) {
   // TODO: Add a listener for click events on the save button. This code should
@@ -23,6 +11,20 @@ $(function (
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  
+  
+  // clickedBtn.on('click', function() {
+  //   var logTxt = $('.description').val();
+  //   console.log(logTxt);
+
+  clickedBtn.on('click', function() {
+    if ($(this).parent().is('#hour-10')) {
+      console.log("working in hour 10")
+    } else {
+      console.log('working but not in hour 10')
+    }
+  })
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -37,5 +39,7 @@ $(function (
   // TODO: Add code to display the current date in the header of the page.
   var today = dayjs().format('dddd, MMMM YYYY');
   var currentDay = $('#currentDay');
-  currentDay.text(today);
+  var currentHour = dayjs().format('H');
+
+  currentDay.text(today + ' ' + currentHour);
 });
