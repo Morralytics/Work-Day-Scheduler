749626e9a8c094aea38cf46852856816a322811d
Nick Morris <nicholasmorris2599@gmail.com>
2022-11-20 18:52:24 -0800

    Added notes to js file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+// Saved JQuery selectors
 var clickedBtn = $('.saveBtn');
 var userInputArea = $('textarea');
-
+// This is used to get all the individual div of hours
 var timeID = $('.container-fluid').children('.time-block');
 
-// var timeIDArr = [];
-
 var notes = [];
-
 var variable = '';
-
-var txt = '';
-
 var eventHourIDArr = [];
+
+
 console.log(timeID);
+
+// Used to set the storage outside of the main function so that it would be able to display on reload
+// Did not get working
 function setStorage() {
   variable = JSON.parse(localStorage.getItem('eventNote'));
   notes.push(variable);
@@ -56,7 +57,8 @@ $(function (
   //     renderStoredNotes();
   // }
 
-
+// Triggers an event when the button element is clicked
+// Traversed the DOM using this to target each specific value of note and hour to use later
   clickedBtn.on('click', function() {
     var eventNote = $(this).parent().children().eq(1).val();
     var eventHourID = $(this).parent().attr('id');
@@ -98,39 +100,40 @@ $(function (
   // renderStoredNotes();
 });
 
+// Implication of dayjs to format the current date and hour for tracking
 var today = dayjs().format('dddd, MMMM YYYY');
 var currentDay = $('#currentDay');
 var currentHour = dayjs().format('H');
 
-currentDay.text(today + ' ' + currentHour);
+currentDay.text(today);
 var arrForNum = [];
 
-
-
-
-timeID.each(function(index, elements) {
+// Used to loop through the length of the divs and slice either the last string letter or the last two string letters
+// This is what I was trying to use to compare to the current hour of the day
+// Was unable to get this working
+for (i = 0; i < timeID.length; i++) {
   
-  console.log('is this even working');
-})
-// for (i = 0; i < timeID.length; i++) {
-  
-//   arrForNum.push(timeID[i].id.slice(5));
-//   console.log(arrForNum[i]);
-//   // if (arrForNum[i] < currentHour) {
-//     //   timeID.addClass('past');
-//     //   console.log("less than current hour");
-//     //   console.log(arrForNum[i]);
-//     // } 
-//     if (+arrForNum[i] < +currentHour) {
-//         timeID.addClass('past');
-//         console.log('current hour');
-//       } else if (+arrForNum[i] > +currentHour) {
-//         timeID.addClass('future');
-//       } else if (arrForNum[i] == +currentHour) {
-//         timeID.addClass('present');
-//       }
-//       console.log(timeID[i])
-// }
+  arrForNum.push(timeID[i].id.slice(5));
+  console.log(arrForNum[i]);
+  // if (arrForNum[i] < currentHour) {
+    //   timeID.addClass('past');
+    //   console.log("less than current hour");
+    //   console.log(arrForNum[i]);
+    // } 
+
+    // For some reason I was unable to properly compare the values of dayjs and my sliced elements until I parsed the day into a number
+    // From here, I know I have to select the current timeID and set that class to whichever condition
+    // However, I was unable to complete this too
+    if (+arrForNum[i] < +currentHour) {
+        timeID.addClass('past');
+        console.log('current hour');
+      } else if (+arrForNum[i] > +currentHour) {
+        timeID.addClass('future');
+      } else if (arrForNum[i] == +currentHour) {
+        timeID.addClass('present');
+      }
+      console.log(timeID[i])
+}
 
 // if (arrForNum[0] < +currentHour) {
 //   console.log('9am is less than current hour')
@@ -143,3 +146,9 @@ timeID.each(function(index, elements) {
 // }
 
 // console.log(arrForNum);
+
+
+
+// I was unable to get a lot of the functionality working however I have set up a session to go over this project more in depth
+// And also go over the local storage implication as well
+// Disapointed on results however I will be coming back to finish this project after my tutor session
