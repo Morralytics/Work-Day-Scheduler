e4c48360651132ebcb09ac3284005940a529b107
Nick Morris <nicholasmorris2599@gmail.com>
2022-11-22 09:15:44 -0800

    After iterating through using JQuery, I was able to target the id value by setting the local storage to two strings. Allowing me to use the same code to grab the correct key and save EACH time slot as its OWN local storage to pull from.

--- Develop/script.js
+++ Develop/script.js
@@ -13,57 +13,64 @@ var currentDay = $('#currentDay');
 var today = dayjs().format('dddd, MMMM YYYY');
 currentDay.text(today);
 
-// Used to set the storage outside of the main function so that it would be able to display on reload
-// Did not get working
-function setStorage() {
-  variable = JSON.parse(localStorage.getItem('eventNote'));
-  notes.push(variable);
-}
-setStorage();
 
 
 
 // Triggers an event when the button element is clicked
 // Traversed the DOM using this to target each specific value of note and hour to use later
-  clickedBtn.on('click', function() {
-    var eventNote = $(this).siblings('.description').val();
-    var eventHourID = $(this).parent().attr('id').split('hour-')[1];
-
-    console.log(eventNote);
-    console.log(eventHourID)
-    // txt = eventNote;
-    // localStorage.setItem('eventNote', JSON.stringify({time: eventHourID, note: eventNote}));
-  })
-
-  var colorChange = function() {
-    // With the same consideration to the comment below, moved the current hour into this function for iteration
-    var currentHour = dayjs().hour();
-    
-    $('.time-block').each(function (){
+clickedBtn.on('click', function() {
+  var eventNote = $(this).siblings('.description').val();
+  var eventHourID = $(this).parent().attr('id').split('hour-')[1];
   
+  console.log(eventNote);
+  console.log(eventHourID);
+  
+  // Since the items are already strings, I don't need to stringify these items for JSON
+  // Using the eventHourID as the key
+  localStorage.setItem(eventHourID, eventNote);
+})
+
+// Used to set the storage outside of the main function so that it would be able to display on reload
+// Did not get working
+function setStorage() {
+
+  // Maybe try looping through each to get the items?
+  // Otherwise I would have to call this on every seperate item
+  $('.description').each(function() {
+    var storedHour = $(this).parent().attr('id').split('hour-')[1];
+    var storedNote = localStorage.getItem(storedHour);
+  })
+}
+
+var colorChange = function() {
+  // With the same consideration to the comment below, moved the current hour into this function for iteration
+  var currentHour = dayjs().hour();
+  
+  // Using the proper JQuery loop instead of a for loop
+  $('.time-block').each(function (){
     // Moving the timeID into the function instead as a global variable, that way it can be used on iteration properly
-      var timeID = parseInt($(this).attr('id').split('hour-')[1]);
-      console.log(timeID);
-      console.log(currentHour);
-  
-        if (timeID < currentHour) {
-            $(this).removeClass('present');
-            $(this).removeClass('future');
-            $(this).addClass('past');
-          } else if (timeID == currentHour) {
-            $(this).removeClass('past');
-            $(this).removeClass('future');
-            $(this).addClass('present');
-          } else {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
-            $(this).addClass('future');
-          }
+    var timeID = parseInt($(this).attr('id').split('hour-')[1]);
+    
+    // Setting the logic behind the color traversal
+    if (timeID < currentHour) {
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+      $(this).addClass('past');
+    } else if (timeID == currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
   })
-  }
+}
 
-  // Need to call the color change function on load so that it is visible at the start of the page
-  colorChange();
+// Need to call the color change function on load so that it is visible at the start of the page
+colorChange();
+setStorage();
 });
 
 
